9f0366422b95dab61ead1fbc46db24737250bd9b
kasicass <kasicass@gmail.com>
2021-06-15 14:18:51 +0800

    update copy shell

--- copy_include_and_lib.sh
+++ copy_include_and_lib.sh
@@ -1,3 +1,5 @@
+# don't need avdevice, avfilter, so don't copy it.
+
 rm -rf output
 
 mkdir output
@@ -7,25 +9,25 @@ mkdir output/lib
 OUTPUT_INCLUDE=output/include
 OUTPUT_LIB=output/lib
 
-# cp -R /usr/local/include/libavdevice $OUTPUT_INCLUDE/
-cp -R /usr/local/include/libavfilter $OUTPUT_INCLUDE/
-cp -R /usr/local/include/libavformat $OUTPUT_INCLUDE/
 cp -R /usr/local/include/libavcodec $OUTPUT_INCLUDE/
+# cp -R /usr/local/include/libavdevice $OUTPUT_INCLUDE/
+# cp -R /usr/local/include/libavfilter $OUTPUT_INCLUDE/
+cp -R /usr/local/include/libavformat $OUTPUT_INCLUDE/
+cp -R /usr/local/include/libavutil $OUTPUT_INCLUDE/
 cp -R /usr/local/include/libswresample $OUTPUT_INCLUDE/
 cp -R /usr/local/include/libswscale $OUTPUT_INCLUDE/
-cp -R /usr/local/include/libavutil $OUTPUT_INCLUDE/
 
+cp ffmpeg-4.3.2/libavcodec/avcodec-58.dll $OUTPUT_LIB/
+cp ffmpeg-4.3.2/libavcodec/avcodec.lib $OUTPUT_LIB/
 # cp ffmpeg-4.3.2/libavdevice/avdevice.dll $OUTPUT_LIB/avdevice.dll
 # cp ffmpeg-4.3.2/libavdevice/avdevice.lib $OUTPUT_LIB/avdevice.lib
-cp ffmpeg-4.3.2/libavfilter/avfilter.dll $OUTPUT_LIB/avfilter.dll
-cp ffmpeg-4.3.2/libavfilter/avfilter.lib $OUTPUT_LIB/avfilter.lib
-cp ffmpeg-4.3.2/libavformat/avformat.dll $OUTPUT_LIB/avformat.dll
-cp ffmpeg-4.3.2/libavformat/avformat.lib $OUTPUT_LIB/avformat.lib
-cp ffmpeg-4.3.2/libavcodec/avcodec.dll $OUTPUT_LIB/avcodec.dll
-cp ffmpeg-4.3.2/libavcodec/avcodec.lib $OUTPUT_LIB/avcodec.lib
-cp ffmpeg-4.3.2/libswresample/swresample.dll $OUTPUT_LIB/swresample.dll
-cp ffmpeg-4.3.2/libswresample/swresample.lib $OUTPUT_LIB/swresample.lib
-cp ffmpeg-4.3.2/libswscale/swscale.dll $OUTPUT_LIB/swscale.dll
-cp ffmpeg-4.3.2/libswscale/swscale.lib $OUTPUT_LIB/swscale.lib
-cp ffmpeg-4.3.2/libavutil/avutil.dll $OUTPUT_LIB/avutil.dll
-cp ffmpeg-4.3.2/libavutil/avutil.lib $OUTPUT_LIB/avutil.lib
+# cp ffmpeg-4.3.2/libavfilter/avfilter-7.dll $OUTPUT_LIB/
+# cp ffmpeg-4.3.2/libavfilter/avfilter.lib $OUTPUT_LIB/
+cp ffmpeg-4.3.2/libavformat/avformat-58.dll $OUTPUT_LIB/
+cp ffmpeg-4.3.2/libavformat/avformat.lib $OUTPUT_LIB/
+cp ffmpeg-4.3.2/libavutil/avutil-56.dll $OUTPUT_LIB/
+cp ffmpeg-4.3.2/libavutil/avutil.lib $OUTPUT_LIB/
+cp ffmpeg-4.3.2/libswresample/swresample-3.dll $OUTPUT_LIB/
+cp ffmpeg-4.3.2/libswresample/swresample.lib $OUTPUT_LIB/
+cp ffmpeg-4.3.2/libswscale/swscale-5.dll $OUTPUT_LIB/
+cp ffmpeg-4.3.2/libswscale/swscale.lib $OUTPUT_LIB/
